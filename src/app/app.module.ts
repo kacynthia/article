9d99bbfbeb9692222440ef4633349f95c29d967a
kacynthia <cyntjob1@gmail.com>
2020-01-15 12:27:54 +0200

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SuiModule} from 'ng2-semantic-ui';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
+    SuiModule,
     BrowserModule,
     AppRoutingModule
   ],
